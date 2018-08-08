f01ee5ccb4a7365c1ef2668e5838d0bcfae9459d
Raleigh Felton <raleighfelton@gmail.com>
2018-08-08 16:48:57 -0400

    styling and animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="header">
+    <div className="header-group">
+    <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+    <Link to="/corses">Course</Link>
+    <Link to="/downloads">Downloads</Link>
+    <Link to="/workshop">Workshops</Link>
+    <Link to="/buy"><button className="animate-fadeIn">Buy</button></Link>
     </div>
   </div>
 )
