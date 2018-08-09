9f257c77948b548beeb8097b8d5721f1bee38c7e
Raleigh Felton <raleighfelton@gmail.com>
2018-08-09 11:45:46 -0400

    interactions and animations

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import "./header.css"
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div className="header">
-    <div className="header-group">
-    <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-    <Link to="/corses">Course</Link>
-    <Link to="/downloads">Downloads</Link>
-    <Link to="/workshop">Workshops</Link>
-    <Link to="/buy"><button className="animate-fadeIn">Buy</button></Link>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
 )
